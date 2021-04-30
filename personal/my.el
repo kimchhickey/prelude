b4b61e8b40f7cdb7e2b1d36e9dc1c5cc96d1b8e4
Sungmin Yang <kimchhickey@gmail.com>
2021-04-30 15:59:19 +0900

    macos

---
+++ personal/my.el
@@ -0,0 +1,5 @@
+;; ispell이 'ko'가 default여서 계속 에러가 났었음.
+(setq ispell-dictionary "american")
+
+;; mac에서 meta key 지정
+(prelude-swap-meta-and-super)
---
+++ personal/preload/my-pre.el
@@ -0,0 +1,11 @@
+;; enable guru mode
+(guru-global-mode +1)
+
+;; set font and size
+(set-face-attribute 'default nil :font "Hasklug Nerd Font 16" )
+
+;; theme
+(disable-theme 'zenburn)
+(setq prelude-theme 'afternoon)
+
+(setq default-input-method "korean-hangul")
